f9e0085449b4ff2939d47d5a1485b28c1caecca8
Kevin McCoy <kevin@qloog.com>
2013-11-22 01:58:18 +0000

    FREEPBX-6895 #resolve #comment Applied the changes that were contained in the page.isymphony.php file attached to this issue.

--- page.isymphony.php
+++ page.isymphony.php
@@ -3,7 +3,7 @@
  *Name         : page.isymphony.php
  *Author       : Michael Yara
  *Created      : July 2, 2008
- *Last Updated : Sept 28, 2011
+ *Last Updated : October 28, 2013
  *History      : 1.0
  *Purpose      : Information page for iSymphony module
  *Copyright    : 2008 HEHE Enterprises, LLC
@@ -393,11 +393,11 @@ if($serverRunning) {
 
 	//Build action buttons
 	$reloadServerButton = "	<form name=\"isymphony_reload_form\" action=\"config.php?type=setup&display=isymphony$debugUrlAdditions\" method=\"post\">
-								<input type=\"Submit\" name=\"isymphony_reload\" value=\"Reload\">
+								<input type=\"Submit\" name=\"isymphony_reload\" value=\"" . _('Reload') . "\">
 							</form>";
 	$activateLicenseButton = "	<form name=\"isymphony_activate_license_form\" action=\"config.php?type=setup&display=isymphony$debugUrlAdditions\" method=\"post\">
 									<input type=\"text\" name=\"isymphony_license_key\">
-									<input type=\"Submit\" name=\"isymphony_activate_license\" value=\"Activate\">
+									<input type=\"Submit\" name=\"isymphony_activate_license\" value=\"" . _('Activate') . "\">
 								</form>";
 
 	//Close iSymphony connection
@@ -466,120 +466,120 @@ $display = "<script language=\"javascript\">
 			<div class=\"content\">
 				$licenseActivationError
 				<table>
-					<tr><td colspan=\"2\"><h2 id=\"title\">iSymphony</h2></td></tr>
-					<tr><td colspan=\"2\"><h5>Server<hr></h5></td></tr>
+					<tr><td colspan=\"2\"><h2 id=\"title\">" . _('iSymphony') . "</h2></td></tr>
+					<tr><td colspan=\"2\"><h5>" . _('Server') . "<hr></h5></td></tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Status:<span>Displays if the iSymphony server is running.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Status:') . "<span>" . _('Displays if the iSymphony server is running.') . "</span></a></td>
 						<td>$serverRunningDisplay</td>						
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Version:<span>Displays the version of the iSymphony server.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Version:') . "<span>" . _('Displays the version of the iSymphony server.') . "</span></a></td>
 						<td>$versionDisplay</td>						
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Reload Server:<span>Reloads the iSymphony server.</span></a>&nbsp&nbsp</td>
+						<td><a href=\"#\" class=\"info\">" . _('Reload Server:') . "<span>" . _('Reloads the iSymphony server.') . "</span></a>&nbsp&nbsp</td>
 						<td>$reloadServerButton</td>						
 					</tr>
 
-					<tr><td colspan=\"2\"><h5>License<hr></h5></td></tr>
+					<tr><td colspan=\"2\"><h5>" . _('License') . "<hr></h5></td></tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Licensed To:<span>Displays the name of the person or company this server is licensed to.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Licensed To:') . "<span>" . _('Displays the name of the person or company this server is licensed to.') . "</span></a></td>
 						<td>$licensedToDisplay</td>						
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Trial Days:<span>Displays the number of remaining license trial days.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Trial Days:') . "<span>" . _('Displays the number of remaining license trial days.') . "</span></a></td>
 						<td>$licenseTrialDaysDisplay</td>						
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Clients:<span>Displays the number of licensed clients.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Clients:') . "<span>" . _('Displays the number of licensed clients.') . "</span></a></td>
 						<td>$licenseClientsDisplay</td>						
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Queues:<span>Displays the number of licensed queues.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Queues:') . "<span>" . _('Displays the number of licensed queues.') . "</span></a></td>
 						<td>$licenseQueuesDisplay</td>						
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Activate:<span>Activates a license with a given serial key.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Activate:') . "<span>" . _('Activates a license with a given serial key.') . "</span></a></td>
 						<td>$activateLicenseButton</td>						
 					</tr>
 
 					<form name=\"isymphony_settings_form\" id=\"isymphony_settings_form\" action=\"config.php?type=setup&display=isymphony$debugUrlAdditions\" method=\"post\" onsubmit=\"return checkForm();\">
-					<tr><td colspan=\"2\"><h5>Server Settings<hr></h5></td></tr>
+					<tr><td colspan=\"2\"><h5>" . _('Server Settings') . "<hr></h5></td></tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Admin User Name:<span>User name used to login to the administration section in the panel.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Admin User Name') . "<span>" . _('User name used to login to the administration section in the panel.') . "</span></a></td>
 						<td><input size=\"20\" type=\"text\" name=\"isymphony_admin_user_name\" value=\"" . htmlspecialchars($locationInformation['admin_user_name']) . "\"></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Admin Password:<span>Password used to login to the administration section in the panel.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Admin Password:') . "<span>" . _('Password used to login to the administration section in the panel.') . "</span></a></td>
 						<td><input size=\"20\" type=\"text\" name=\"isymphony_admin_password\" value=\"" . htmlspecialchars($locationInformation['admin_password']) . "\"></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Originate Timeout:<span>Number of milliseconds that the originating extension will be rung when placing a call via the panel before timing out.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Originate Timeout:') . "<span>" . _('Number of milliseconds that the originating extension will be rung when placing a call via the panel before timing out.') . "</span></a></td>
 						<td><input size=\"20\" type=\"text\" name=\"isymphony_originate_timeout\" value=\"" . htmlspecialchars($locationInformation['originate_timeout']) . "\"></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Auto Reload:<span>Tells the server to automatically reload the location when the dial plan is reloaded.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Auto Reload:') . "<span>" . _('Tells the server to automatically reload the location when the dial plan is reloaded.') . "</span></a></td>
 						<td><input type=\"checkbox\" name=\"isymphony_auto_reload\"" . (($locationInformation['auto_reload'] == "1") ? " checked" : "") . "></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Enable Page Status:<span>If checked allows you to see paging status on extensions in the panel.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Enable Page Status:') . "<span>" . _('If checked allows you to see paging status on extensions in the panel.') . "</span></a></td>
 						<td><input type=\"checkbox\" name=\"isymphony_page_status_enabled\"" . (($locationInformation['page_status_enabled'] == "1") ? " checked" : "") . "></td>
 					</tr>
 					<tr>
-                    		<td><a href=\"#\" class=\"info\">iSymphony Server Host:<span>IP Address or FQDN of iSymphony Server</span></a></td>
+                    		<td><a href=\"#\" class=\"info\">" . _('iSymphony Server Host:') . "<span>" . _('IP Address or FQDN of iSymphony Server') . "</span></a></td>
                     		<td><input size=\"20\" type=\"text\" name=\"isymphony_admin_host\" value=\"" . htmlspecialchars($locationInformation['isymphony_host']) . "\"></td>
                     </tr>
                     <tr>
-                    		<td><a href=\"#\" class=\"info\">Location:<span>iSymphony Server Location:</span></a></td>
+                    		<td><a href=\"#\" class=\"info\">" . _('Location:') . "<span>" . _('iSymphony Server Location:') . "</span></a></td>
                     		<td><input size=\"20\" type=\"text\" name=\"isymphony_admin_location\" value=\"" . htmlspecialchars($locationInformation['isymphony_location']) . "\"></td>
                     </tr>
 					<tr>
-                    		<td><a href=\"#\" class=\"info\">Tenant:<span>iSymphonyServer Tenant:</span></a></td>
+                    		<td><a href=\"#\" class=\"info\">" . _('Tenant:') . "<span>" . _('iSymphonyServer Tenant:') . "</span></a></td>
                     		<td><input size=\"20\" type=\"text\" name=\"isymphony_admin_tenant\" value=\"" . htmlspecialchars($locationInformation['isymphony_tenant']) . "\"></td>
                     </tr>
                     <tr>
-                    		<td><a href=\"#\" class=\"info\">Client Port:<span>Sets the port on the server that serves the client files.</span></a></td>
+                    		<td><a href=\"#\" class=\"info\">" . _('Client Port:') . "<span>" . _('Sets the port on the server that serves the client files.') . "</span></a></td>
                     		<td><input size=\"20\" type=\"text\" name=\"isymphony_client_port\" value=\"" . htmlspecialchars($locationInformation['isymphony_client_port']) . "\"></td>
                     </tr>
 					<tr>
-                            <td><a href=\"#\" class=\"info\">Asterisk Server Host:<span>IP Address or FQDN of your Asterisk Server</span></a></td>
+                            <td><a href=\"#\" class=\"info\">" . _('Asterisk Server Host:') . "<span>" . _('IP Address or FQDN of your Asterisk Server') . "</span></a></td>
                     		<td><input size=\"20\" type=\"text\" name=\"isymphony_asterisk_host\" value=\"" . htmlspecialchars($locationInformation['asterisk_host']) . "\"></td>
                      </tr>
-					<tr><td colspan=\"2\"><h5>Global Jabber Settings<hr></h5></td></tr>
+					<tr><td colspan=\"2\"><h5>" . _('Global Jabber Settings') . "<hr></h5></td></tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Host:<span>Jabber host to be used unless otherwise overridden by the extension settings.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Host:') . "<span>" . _('Jabber host to be used unless otherwise overridden by the extension settings.') . "</span></a></td>
 						<td><input size=\"20\" type=\"text\" name=\"isymphony_jabber_host\" value=\"" . htmlspecialchars($locationInformation['jabber_host']) . "\"></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Domain:<span>Jabber domain to be used unless otherwise overridden by the extension settings.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Domain:') . "<span>" . _('Jabber domain to be used unless otherwise overridden by the extension settings.') . "</span></a></td>
 						<td><input size=\"20\" type=\"text\" name=\"isymphony_jabber_domain\" value=\"" . htmlspecialchars($locationInformation['jabber_domain']) . "\"></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Resource:<span>Jabber resource to be used unless otherwise overridden by the extension settings.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Resource:') . "<span>" . _('Jabber resource to be used unless otherwise overridden by the extension settings.') . "</span></a></td>
 						<td><input size=\"20\" type=\"text\" name=\"isymphony_jabber_resource\" value=\"" . htmlspecialchars($locationInformation['jabber_resource']) . "\"></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Port:<span>Jabber port to be used unless otherwise overridden by the extension settings.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Port:') . "<span>" . _('Jabber port to be used unless otherwise overridden by the extension settings.') . "</span></a></td>
 						<td><input size=\"20\" type=\"text\" name=\"isymphony_jabber_port\" value=\"" . htmlspecialchars($locationInformation['jabber_port']) . "\"></td>
 					</tr>
 					<tr>
-						<td><a href=\"#\" class=\"info\">Log Messages:<span>Tells the server to log all chat conversations.</span></a></td>
+						<td><a href=\"#\" class=\"info\">" . _('Log Messages:') . "<span>" . _('Tells the server to log all chat conversations.') . "</span></a></td>
 						<td><input type=\"checkbox\" name=\"isymphony_log_jabber_messages\"" . (($locationInformation['log_jabber_messages'] == "1") ? " checked" : "") . "></td>
 					</tr>
 					<tr>
-						<td colspan=\"2\"><input type=\"Submit\" name=\"isymphony_update_location_settings\" value=\"Submit Changes\"></td>
+						<td colspan=\"2\"><input type=\"Submit\" name=\"isymphony_update_location_settings\" value=\"" . _('Submit Changes') . "\"></td>
 					</tr>
 					</form>
-					<tr><td colspan=\"2\"><h5>Module Debug<hr></h5></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showmoduledebuglog=yes$debugUrlAdditions\">View Debug Log</a></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showmoduleerrorlog=yes$debugUrlAdditions\">View Error Log</a></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showmoduledb=yes$debugUrlAdditions\">View Database</a></td></tr>
-					<tr><td colspan=\"2\"><h5>Server Debug<hr></h5></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showservererrorlog=yes$debugUrlAdditions\">View Error Log</a></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showservercorelog=yes$debugUrlAdditions\">View Core Log</a></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showservercommunicationlog=yes$debugUrlAdditions\">View Communication Log</a></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showserverajexternallog=yes$debugUrlAdditions\">View AMI External Log</a></td></tr>
-					<tr><td><a href=\"config.php?type=setup&display=isymphony&showserverajinternallog=yes$debugUrlAdditions\">View AMI Internal Log</a></td></tr>		
+					<tr><td colspan=\"2\"><h5>" . _('Module Debug') . "<hr></h5></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showmoduledebuglog=yes$debugUrlAdditions\">" . _('View Debug Log') . "</a></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showmoduleerrorlog=yes$debugUrlAdditions\">" . _('View Error Log') . "</a></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showmoduledb=yes$debugUrlAdditions\">" . _('View Database') . "</a></td></tr>
+					<tr><td colspan=\"2\"><h5>" . _('Server Debug') . "<hr></h5></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showservererrorlog=yes$debugUrlAdditions\">" . _('View Error Log') . "</a></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showservercorelog=yes$debugUrlAdditions\">" . _('View Core Log') . "</a></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showservercommunicationlog=yes$debugUrlAdditions\">" . _('View Communication Log') . "</a></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showserverajexternallog=yes$debugUrlAdditions\">" . _('View AMI External Log') . "</a></td></tr>
+					<tr><td><a href=\"config.php?type=setup&display=isymphony&showserverajinternallog=yes$debugUrlAdditions\">" . _('View AMI Internal Log') . "</a></td></tr>		
 					$debugLogDisplay
 				</table>
 				$databaseDisplay
